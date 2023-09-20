fcfca6c3a2b7a88971e12dee643be77ebfb40558
SHANELLY000 <145599134+SHANELLY000@users.noreply.github.com>
2023-09-20 12:49:40 -0600

    Add files via upload

---
+++ Diseño Web/ExamenShania/java.js
@@ -0,0 +1,47 @@
+
+// Utilizando la palabra clave var
+var nombre = "Shania";
+
+// Utilizando la palabra clave let
+let nombre = "Shania";
+
+// Utilizando la palabra clave const (constante)
+const nombre = "Shania";
+
+
+var edad = 25;
+console.log(edad);
+
+var esEstudiante = true;
+var esEstudiante = false;
+var esEstudiante = true;
+console.log(esEstudiante);
+
+
+function sumar(a, b) {
+    if (a > b) {
+    return true;
+    } else {
+    return false;
+    }
+
+}var resultado = sumar(5, 3);
+  console.log(resultado); // Imprimirá "true" en la consola
+
+function esMayor(a, b) {
+    return a > b;
+}
+
+var resultado = esMayor(5, 3);
+console.log(resultado); // Imprimirá "true" en la consola
+
+var num = 42;
+
+if (num > 0) {
+console.log("El número es positivo.");
+} else if (num < 0) {
+console.log("El número es negativo.");
+} else {
+console.log("El número es cero.");
+}
+
